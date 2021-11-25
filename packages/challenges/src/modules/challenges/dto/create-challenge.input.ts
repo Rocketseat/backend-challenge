3d2c07f82e17325ce0d1fd241b4d4a0790265a36
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-11-25 01:39:49 -0300

    feat: inclued validation in fields

--- packages/challenges/src/modules/challenges/dto/create-challenge.input.ts
+++ packages/challenges/src/modules/challenges/dto/create-challenge.input.ts
@@ -1,11 +1,14 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
 import { IChallenge } from '../interfaces/IChallenge.interface';
 
 @InputType()
 export class CreateChallengeInput implements Partial<IChallenge> {
   @Field()
+  @IsString()
   title: string;
 
   @Field()
+  @IsString()
   description: string;
 }
