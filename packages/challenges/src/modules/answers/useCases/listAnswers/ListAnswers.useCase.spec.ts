30e7f694d5af60c59fc108be0a9e666ce1c29b64
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-12-05 00:39:07 -0300

    test: should be list all answers

---
+++ packages/challenges/src/modules/answers/useCases/listAnswers/ListAnswers.useCase.spec.ts
@@ -0,0 +1,52 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Status } from '../../enums/Status.enum';
+import { IAnswerRepository } from '../../repositories/IAnswer.repository';
+import { AnswerInMemoryRepository } from '../../repositories/inMemory/AnswerInMemory.repository';
+import { AnswerRepository } from '../../repositories/prisma/Answer.repository';
+import { ListAnswersUseCase } from './ListAnswers.useCase';
+
+describe('List Answers Use Case', () => {
+  let answerRepository: IAnswerRepository;
+  let listAnswersUseCase: any;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
+      providers: [
+        ListAnswersUseCase,
+        {
+          provide: AnswerRepository,
+          useClass: AnswerInMemoryRepository,
+        },
+      ],
+    }).compile();
+
+    answerRepository = module.get<IAnswerRepository>(AnswerRepository);
+    listAnswersUseCase = module.get<ListAnswersUseCase>(ListAnswersUseCase);
+  });
+
+  it('should be defined', () => {
+    expect(listAnswersUseCase).toBeDefined();
+  });
+
+  it('should be list all answers', async () => {
+    const [firstAnswer, secondAnswer] = await Promise.all([
+      answerRepository.create({
+        challengeId: '1',
+        link: 'https://github.com/jorge-lba/ignite-tests-challenge',
+        grade: 8,
+        status: Status.Done,
+      }),
+      answerRepository.create({
+        challengeId: '2',
+        link: 'https://github.com/',
+        grade: null,
+        status: Status.Error,
+      }),
+    ]);
+
+    const answers = await listAnswersUseCase.execute();
+
+    expect(answers).toEqual([firstAnswer, secondAnswer]);
+  });
+});
