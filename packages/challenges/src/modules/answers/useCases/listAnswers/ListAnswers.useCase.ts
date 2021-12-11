809a405c5ebaefd41b616929aeaa0c82c6b8d406
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-12-10 22:52:58 -0300

    feat: implemented pagination to list awnsers

--- packages/challenges/src/modules/answers/useCases/listAnswers/ListAnswers.useCase.ts
+++ packages/challenges/src/modules/answers/useCases/listAnswers/ListAnswers.useCase.ts
@@ -1,13 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { IAnswer } from '../../interfaces/IAnswer.interface';
+import { PaginationInput } from 'src/modules/challenges/dto/pagination.input';
+import { IPagination } from 'src/modules/challenges/interfaces/IPagination.interface';
+import {
+  IFilter,
+  IListAllResponse,
+} from '../../repositories/IAnswer.repository';
 import { AnswerRepository } from '../../repositories/prisma/Answer.repository';
 
+interface IListAnswersInput {
+  pagination?: IPagination;
+  filter?: IFilter;
+}
 @Injectable()
 export class ListAnswersUseCase {
   constructor(private readonly answerRepository: AnswerRepository) {}
 
-  async execute(): Promise<IAnswer[]> {
-    const answers = await this.answerRepository.findAll();
+  async execute(
+    listAnswersInput: IListAnswersInput,
+  ): Promise<IListAllResponse> {
+    const DEFAULT_PAGINATION = {
+      page: 1,
+      pageSize: 10,
+    } as PaginationInput;
+
+    const answers = await this.answerRepository.findAll(
+      {
+        ...DEFAULT_PAGINATION,
+        ...listAnswersInput?.pagination,
+      },
+      listAnswersInput?.filter,
+    );
+
     return answers;
   }
 }
