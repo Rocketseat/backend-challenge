9422183416a87ef4a4051438ec2f6f0c8ec4c3a9
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-12-11 01:31:27 -0300

    feat: import resolver to module

--- packages/challenges/src/modules/answers/answers.module.ts
+++ packages/challenges/src/modules/answers/answers.module.ts
@@ -8,21 +8,27 @@ import { ChallengesModule } from '../challenges/challenges.module';
 import { Transport, ClientsModule, ClientKafka } from '@nestjs/microservices';
 import { UpdateAnswerUseCase } from './useCases/updateAnswer/UpdateAnswer.useCase';
 import { ListAnswersUseCase } from './useCases/listAnswers/ListAnswers.useCase';
+import { ListAnswersResolver } from './useCases/listAnswers/ListAnswers.resolver';
+import { UpdateAnswerController } from './useCases/updateAnswer/UpdateAnswer.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     HttpModule,
     ChallengesModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     ClientsModule.register([
       {
         name: 'answer-kafka',
         transport: Transport.KAFKA,
         options: {
           client: {
-            brokers: ['localhost:9092'],
+            brokers: [process.env.KAFKA_BROKER],
           },
           consumer: {
-            groupId: 'answer-producer-' + Date.now(),
+            groupId: 'answer-consumer',
           },
         },
       },
@@ -34,8 +40,11 @@ import { ListAnswersUseCase } from './useCases/listAnswers/ListAnswers.useCase';
     SendAnswerUseCase,
     SendAnswerResolver,
     ListAnswersUseCase,
+    ListAnswersResolver,
+    UpdateAnswerUseCase,
     ClientKafka,
   ],
-  controllers: [UpdateAnswerUseCase],
+  controllers: [UpdateAnswerController],
+  exports: [AnswerRepository],
 })
 export class AnswersModule {}
